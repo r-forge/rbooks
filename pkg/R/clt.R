e3be50c7b1dcb86a1189ca7d53616d9e40eaa01d
phewson (Dr ) <paul.hewson@plymouth.ac.uk>
2008-10-06 13:36:21 +0000

    This update hopefully fixes the problem with the math notation not showing.  With rather an aggresive fix.   All code has been commented out - need to check if that fixes the infinite browser opening issue.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rbooks@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clt.R
+++ pkg/R/clt.R
@@ -3,24 +3,28 @@ function(overwrite = FALSE){
 require(Rpad)
 ## a check to see whether the clt.Rpad file already exists in the working directory
 dir.data <- dir()
-if(length(grep("clt.Rpad", dir.data)) == 0) {replacenotes()}
-## Above and below could be put together
-if(overwrite == TRUE) {replacenotes()}
+if(length(grep("clt.Rpad", dir.data)) == 0) {cat("\n \n You appear to have no files in your working directory.  \n \n If you are sure this is the working directory you wish to use, \n \n you may wish to set the overwrite option to TRUE and start again \n \n \n ")}
+
+if(overwrite == TRUE) {replacenotes()
 ## a horrible kludge, but this tries to stop the server in order that the next command can start it again (and it does complain about it)
-try(stopRpadServer(), silent = TRUE)
+try(stopRpadServer(), silent = TRUE)}
 ## load the Rbook in Rpad
-Rpad("clt.Rpad")
+if(length(grep("clt.Rpad", dir.data)) == 0){
+Rpad("clt.Rpad")}
 }
 
 `replacenotes` <- 
 function(){
 ## load the Rbook
 data(cltbook)
-## figure out where the images are stored
-pkgs <- installed.packages()
-imgpath <- paste(pkgs[pkgs[,1]=="Rbooks",2], "/Rbooks/img", sep = "")
-cltbook2 <- sub("imgpath", imgpath, cltbook)
-writeLines(cltbook2, "clt.Rpad")
+writeLines(cltbook, "clt.Rpad")
+
+require(biOps)
+data(cltimg)
+for (i in 1:24){
+eval(parse(text = paste("writeJpeg(\"cltimg", i, ".jpeg\", cltimg[[", i, "]])", sep = "")))
+}
+
 }
 
 
